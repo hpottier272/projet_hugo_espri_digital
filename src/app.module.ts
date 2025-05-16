bb11e46f2b11646176b35465b494a1f09cb4afe8
hpottier272 <hugo272@orange.com>
2025-05-16 18:13:59 +0200

    creation dosier users et installation de type ORM pour echange avec bdd mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController} from './app.controller';
 import { AppService } from './app.service';
 import { AuthentificationModule } from './authentification/authentification.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { User } from './users/user.entity';
+import { UserModule } from './users/user.module';
 
 @Module({
-  imports: [AuthentificationModule],
+  imports: [
+    AuthentificationModule, 
+    UserModule,
+    TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: 'root',
+        database: 'test',
+        entities: [User],
+        synchronize: true, 
+        retryAttempts : 3,
+        retryDelay : 3000000,
+        autoLoadEntities: true,
+    }), 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
